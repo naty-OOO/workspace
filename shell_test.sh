197c1c6627d4e1f2fd12dc41d7e21b17350cd5d1
licxxa <licxxa@syntronic.com>
2021-07-21 17:59:28 +0800

    add code about peocess control

--- shell_test.sh
+++ shell_test.sh
@@ -22,6 +22,8 @@ echo ${#name}
 echo ${name:0:5}
 echo $name $a
 !
+#数组
+:<<!
 array=(
 001
 002
@@ -43,6 +45,7 @@ echo "执行文件名：$0";
 echo "第一个参数：$1";
 echo "第二个参数：$2";
 echo "第三个参数：$3";
+!
 :<<!
 echo "参数个数：$#";
 echo "字符串显示：$*";
@@ -132,6 +135,7 @@ echo  -e "name is \n$name" > output.txt
 echo -e "name is \c"
 echo "$name"
 !
+:<<!
 echo '$name\'
 echo `date`
 printf "hello\n"
@@ -139,3 +143,90 @@ printf "%-s %-s %-s\n" 姓名 性别 体重kg
 printf "%-1s %-8s %-4.2f\n" 郭靖1 男 66.1234
 printf "%-10s %-8s %-4.2f\n" 杨过 男 48.6543
 printf "%-10s %-8s %-4.2f\n" 郭芙 女 47.9876
+!
+:<<!
+echo "请输入两个数"
+read num1 num2
+if [ $num1 == $num2 ]
+then 
+	echo "$num1 等于 $num2"
+elif [ $num1 -gt $num2 ]
+then 
+	echo "$num1 大于 $num2"
+else 
+	echo "$num1 小于 $num2"
+fi
+sum=$[num1+num2]
+echo "和为$sum"
+!
+:<<!
+for i in 1 2 3 4 5
+do
+	echo "$i"
+done
+for j in hello!
+do 
+	echo "$j"
+done
+
+read n
+i=0
+while(($i <= $n))
+do
+	echo $i
+	#let "i++"
+	#i=$[i+1];
+	i=`expr $i + 1`
+done
+echo '按下<ctrl -D>退出'
+echo -n '输入网址：'
+while read FILM
+do 
+	echo "$FILM"
+done
+!
+:<<!
+i=0
+until [ $i -eq 1000 ]
+do
+	case $i in
+	10) 
+	echo $i 
+	;;
+	20) 
+	echo $i 
+	;;
+	esac
+	let "i++"
+done
+!
+casefun(){
+echo "请输入数字"
+read num
+case "$num" in
+	"a") echo "您输入的是a"
+	;;
+	"b") echo "b您输入的是b"
+	;;
+	"c") echo "您输入的是c"
+	;;
+	"d") echo "您输入的是d"
+	;;
+	"e") echo "您输入的是e"
+	;;
+	*)   echo "error!!!!"
+	;;
+esac
+	echo "第一个参数：$1"
+	echo "第一个参数：$2"
+	echo "第一个参数：$3"
+	echo "第一个参数：${10}"
+	echo "$*"
+	return $num
+}
+echo "start-----------"
+casefun a b c d e f g h i j k l m 
+echo "返回值：$?"
+echo "end--------------"
+
+
